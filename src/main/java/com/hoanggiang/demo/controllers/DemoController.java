d3f111944f1761b6f0c8636615f85ce785298924
lhggiang <luutru040204@gmail.com>
2024-11-10 21:28:18 +0700

    Bean Lifecycle

---
+++ src/main/java/com/hoanggiang/demo/controllers/DemoController.java
@@ -0,0 +1,19 @@
+package com.hoanggiang.demo.controllers;
+
+import com.hoanggiang.demo.services.EmailService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+//    private final EmailService emailService;
+//
+//    public DemoController(EmailService emailService) {
+//        this.emailService = emailService;
+//    }
+
+//    @GetMapping
+//    public String getEmail(){
+//        return "This is your email";
+//    }
+}
---
+++ src/main/java/com/hoanggiang/demo/services/EmailService.java
@@ -0,0 +1,24 @@
+package com.hoanggiang.demo.services;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    public EmailService() {
+        System.out.println("Email Service created");
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("Email Service initialized");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("Email Service destroyed");
+    }
+
+}
